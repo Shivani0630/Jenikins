d66b04102e197d74dfe7facf22500db3fa60b6b2
Shivani0630 <Shivani0630>
2023-08-18 14:48:12 +0530

    second

---
+++ src/test/java/com/practice/secondLogintest.java
@@ -0,0 +1,12 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class secondLogintest {
+	@Test
+	public void Logintest()
+	{
+		System.out.println("----i hate jenkins----");
+	}
+
+}
