f8c97f5b52e38a25941d4ad5350795ec72a638c8
shivani0630 <shivuishu0630@gmail.com>
2023-08-18 12:37:23 +0530

    done

---
+++ src/test/java/com/practice/LoginTest.java
@@ -0,0 +1,13 @@
+package com.practice;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	@Test
+	
+	public void logintest()
+	{
+		System.out.println("--- logeed in to app ----");
+	}
+}
